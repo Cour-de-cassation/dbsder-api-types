1b53e5bbc1616d409747d46d0e57f3da9b2b8075
Julien Grach <julien.grach@justice.fr>
2025-05-28 13:32:33 +0200

    add cph skeleton

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,11 +3,14 @@ import { DecisionCa, UnIdentifiedDecisionCa } from './decisions_ca'
 import { DecisionCc, UnIdentifiedDecisionCc } from './decisions_cc'
 import { DecisionDila, UnIdentifiedDecisionDila } from './decisions_dila'
 import { DecisionTcom, UnIdentifiedDecisionTcom } from './decisions_tcom'
+import { DecisionCph, UnIdentifiedDecisionCph } from './decisions_cph'
 
 export { DecisionTj, UnIdentifiedDecisionTj } from './decisions_tj'
 export { DecisionCa, UnIdentifiedDecisionCa } from './decisions_ca'
 export { DecisionCc, UnIdentifiedDecisionCc } from './decisions_cc'
 export { DecisionDila, UnIdentifiedDecisionDila } from './decisions_dila'
+export { DecisionCph, UnIdentifiedDecisionCph } from './decisions_cph'
+
 export {
   DecisionTcom,
   UnIdentifiedDecisionTcom,
@@ -28,10 +31,11 @@ export {
   Category
 } from './common'
 
-export type Decision = DecisionTj | DecisionTcom | DecisionCa | DecisionCc | DecisionDila
+export type Decision = DecisionTj | DecisionTcom | DecisionCa | DecisionCc | DecisionDila | DecisionCph
 export type UnIdentifiedDecision =
   | UnIdentifiedDecisionTj
   | UnIdentifiedDecisionTcom
   | UnIdentifiedDecisionCa
   | UnIdentifiedDecisionCc
   | UnIdentifiedDecisionDila
+  | UnIdentifiedDecisionCph
