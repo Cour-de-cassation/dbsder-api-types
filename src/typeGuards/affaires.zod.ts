6cc5dddc1e6bcdae410e48dc655c4133f7d38320
YATERA Boubacar <boubacar.yatera@justice.fr>
2025-10-07 12:20:04 +0200

    adding typeguard affaires and changing decisions to decisionIds

---
+++ src/typeGuards/affaires.zod.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod'
+import { Category } from '../types/common'
+import { Affaire, UnIdentifiedAffaire } from '../types/affaire'
+import { zObjectId } from './common.zod'
+
+
+export const zCategory = z.enum(Category)
+
+export const zReplacementTerms = z.object({
+    entityId: z.string(),
+    replacementTerm: z.string(),
+    origialTextInstances: z.array(z.string()),
+    category: zCategory
+})
+
+export const zAffaireSchema = z.object({
+    _id: zObjectId,
+    replacementTerms: z.array(zReplacementTerms),
+    decisionIds: z.array(zObjectId),
+    numeroPourvois: z.array(z.string())
+})
+
+export function parseAffaire(x: unknown): UnIdentifiedAffaire {
+    return zAffaireSchema.omit({ _id: true }).parse(x)
+}
+
+export function parsePartialAffaire(x: unknown): Partial<Affaire> {
+    return zAffaireSchema.partial().parse(x)
+}
