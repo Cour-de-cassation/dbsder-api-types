c48113547c547b7c3e07573cc5894da24390ea46
mauryaland <amaury@fouret.org>
2026-03-13 10:21:01 +0100

    add additional document types to DocumentAssocieType enum (#91)

--- src/typeGuards/documentsAssocies.zod.ts
+++ src/typeGuards/documentsAssocies.zod.ts
@@ -3,7 +3,12 @@ import { zLabelTreatments, zObjectId } from './common.zod'
 
 export enum DocumentAssocieType {
   AVIS = 'avis',
-  RAPPORT = 'rapport'
+  AVIS_QPC = 'avis QPC',
+  AVIS_COMP = 'avis complémentaire',
+  RAPPORT = 'rapport',
+  RAPPORT_QPC = 'rapport QPC',
+  RAPPORT_COMP = 'rapport complémentaire',
+
 }
 
 export const documentAssocieTypeSchema = z.enum(DocumentAssocieType)
